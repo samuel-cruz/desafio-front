e9d038a4cc7b731ca938d0205725eefabf7f1d89
samuel-cruz <samuel.sdacruz@gmail.com>
2020-09-25 00:59:38 -0300

    Configuração de porta

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, PUBLIC_DIR),
     hot: true,
-    port: 3340,
+    port: process.env.PORT || 3340,
   },
   entry: path.resolve(__dirname, "src", "main.js"),
   mode: "development",
